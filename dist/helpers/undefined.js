f37f246bdd5d1dfe72156676b7ee52ba785c1b90
lorinc <lorinc.toldi@gmail.com>
2024-02-09 15:45:03 +0100

    Remove dist directory from version control

--- dist/helpers/undefined.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const fixUndefined = (data, base) => {
-    for (let key of Object.keys(base))
-        if (data[key] === undefined)
-            data[key] = null;
-    return data;
-};
-exports.default = fixUndefined;
